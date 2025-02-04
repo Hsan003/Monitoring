fce34ae797dc213a11fec97f5535bfc5552fdebf
MouhibBahri <mouhib.bahri@insat.ucar.tn>
2025-02-04 18:48:39 +0100

    fix email service

--- Monitoring/Models/NotificationsModule/NotificationsChannels/EmailChannel.cs
+++ Monitoring/Models/NotificationsModule/NotificationsChannels/EmailChannel.cs
@@ -1,48 +1,34 @@
+using System.Net;
 using System.Net.Mail;
+using System.Threading.Tasks;
 using Monitoring.Models;
 using Monitoring.Models.NotificationsModule.NotificationsChannels;
 
 public class EmailChannel : INotificationChannel
 {
-    private readonly string smtpServer;
-    private readonly int smtpPort;
-    private readonly string senderEmail;
-    private readonly string senderPassword;
-
-    public EmailChannel(string smtpServer, int smtpPort, string senderEmail, string senderPassword)
-    {
-        this.smtpServer = smtpServer;
-        this.smtpPort = smtpPort;
-        this.senderEmail = senderEmail;
-        this.senderPassword = senderPassword;
-    }
-
     public async Task<bool> SendNotification(string content, Client recipient)
     {
-        
         try
         {
-            string smtpServer = Environment.GetEnvironmentVariable("SMTP_SERVER") ?? "localhost";
-            int smtpPort = int.Parse(Environment.GetEnvironmentVariable("SMTP_PORT") ?? "25");
-            string senderEmail = Environment.GetEnvironmentVariable("SENDER_EMAIL") ?? "no-reply@example.com";
-            string senderPassword = Environment.GetEnvironmentVariable("SENDER_PASSWORD") ?? "";
+            // Read SMTP configuration from environment variables
+            string smtpServer = Environment.GetEnvironmentVariable("SMTP_SERVER") ;
+            int smtpPort = int.Parse(Environment.GetEnvironmentVariable("SMTP_PORT")) ;
+            string senderEmail = Environment.GetEnvironmentVariable("SENDER_EMAIL");
+            string senderPassword = Environment.GetEnvironmentVariable("SENDER_PASSWORD");
 
-            using (var smtpClient = new SmtpClient(smtpServer, smtpPort))
+            var client = new SmtpClient(smtpServer, smtpPort)
             {
-                smtpClient.Credentials = !string.IsNullOrWhiteSpace(senderPassword)
-                    ? new System.Net.NetworkCredential(senderEmail, senderPassword)
-                    : null;
-                smtpClient.EnableSsl = smtpPort == 587 || smtpPort == 465; // SSL only if using standard SMTP secure ports
 
-                var mailMessage = new MailMessage(senderEmail, recipient.Email)
-                {
-                    Subject = "Notification",
-                    Body = content
-                };
+                Credentials = new NetworkCredential(senderEmail, senderPassword),
 
-                await smtpClient.SendMailAsync(mailMessage);
+                EnableSsl = true
+
+            };
+
+            client.Send(senderEmail,recipient.Email , "Uptime Monitoring API Notif", content);
+
                 return true;
-            }
+            
         }
         catch (Exception ex)
         {
